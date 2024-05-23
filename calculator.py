8a1d06beeb9990002ee2c7c9d067f810140d7065
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-23 11:18:15 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# a = 50
+# b = 3
+
+# print("The value of",a, "+", 3, "is: ", a+b)
+# print("The value of",a, "-", 3, "is: ", a-b)
+# print("The value of",a, "*", 3, "is: ", a*b)
+# print("The value of",a, "/", 3, "is: ", a/b)
+# print("The value of",a, "%", 3, "is: ", a%b)  
+
+a = "1"
+b = "2"
+print(int(a) + int(b))
+ 
+#  implicit type casting
+
+
+c = 1.9
+d = 8
+print(c + d  )
