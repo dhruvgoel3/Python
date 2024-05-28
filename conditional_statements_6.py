c1b4221d7b0f718468184fe39a7919428d46e4f1
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-28 23:10:10 +0530

    Conditional Statements in python

---
+++ conditional_statements_6.py
@@ -0,0 +1,45 @@
+#----CONDITIONAL OPERATORS----
+#  [ < , > , <= , >= , == , != ]
+
+
+
+# a = int(input("Enter your age"))
+# print("Your age is:", a)
+
+# if(a>18):
+#     print("You can drive")
+# else:
+#     print("You can not drive") 
+
+
+# applePrice = 10
+# budget = 200
+# if(budget - applePrice > 300 ):
+#      print("Alexa, add 1kg apples to the cart.")
+# elif(budget - applePrice > 70):
+#      print("Its okk you can buy it")
+# else:
+#      print("Akexa, do not add apples to the cart" )
+
+
+
+#------------A GOOD EXAMPLE OF {elif statement}-------------------
+
+
+# num = int(input("Enter the value of the num"))
+# if(num<0):
+#     print("Number is Negative")
+# elif(num==0):
+#     print("Number is zero")
+# else:
+#     print("Number is possitive")
+
+
+# ---------- NESTED IF STATEMENTS-----------
+
+
+
+
+
+
+
