90486905250f94ea81252e6a52b5a49def42a5cf
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-29 16:29:44 +0530

    forloop

---
+++ forloop_8.py
@@ -0,0 +1,14 @@
+name = 'Abhishek'
+for i in name:
+    print(i)
+    if(i=="b"):
+        print("There is something special")
+
+colors = ["Red" , "Green" , "Blue" , "Yellow" ]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+for k in range(1 , 20000):
+   print(k)
