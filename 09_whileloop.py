94484bc948f2d84735cdde80c719f0bc6a623ebb
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-31 22:44:22 +0530

    while loop

---
+++ 09_whileloop.py
@@ -0,0 +1,15 @@
+# i = 0
+# while(i<3):
+#     print(i)
+#     i = i + 1
+
+
+# i = 0
+# while(i<=20):
+#     print(i)
+#     i = i +1
+
+i = int(input("Enter the number: "))
+while(i<=30):
+    i = int(input("Enter the number: "))
+    print(i)
